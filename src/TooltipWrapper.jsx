58bcd7af9798fc54e82b5b89d8cb350169f54871
habit <ayomidebakare301@gmail.com>
2021-03-18 17:01:56 +0100

    Created TooltipWrapper.jsx
    -NB: Use this for EditingEnvironment's buttons

---
+++ src/TooltipWrapper.jsx
@@ -0,0 +1,52 @@
+import { useState, useRef } from "react";
+import { Children } from "react";
+import { Overlay, Tooltip } from "react-bootstrap";
+
+const Target = (targetProps) => targetProps.children;
+
+const Tip = (tipProps) => tipProps.children;
+
+function TooltipWrapper(props) {
+	let [show, setShow] = useState(false);
+	let targetRef = useRef();
+
+    const toggleShow = () => {
+        setShow(!show)
+    }
+
+	return (
+		<div>
+			{Children.toArray(props.children).map((c, i) => {
+				if (c.type === Target) {
+					return (
+						<div
+                            className="inline-block"
+							key={i}
+							ref={targetRef}
+							onClick={c.props.click ? toggleShow : null}
+                            onMouseOver={c.props.hover ? () => setShow(true) : null}
+                            onMouseOut={c.props.hover ? () => setShow(false) : null}
+						>
+							{c}
+						</div>
+					);
+				} else if (c.type === Tip)
+					return (
+						<Overlay key={i} target={targetRef.current} show={show} placement="right">
+							{(props) => (
+								<Tooltip id="overlay-example" {...props}>
+									{c}
+								</Tooltip>
+							)}
+						</Overlay>
+					);
+				else return c;
+			})}
+		</div>
+	);
+}
+
+TooltipWrapper.Target = Target;
+TooltipWrapper.Tip = Tip;
+
+export default TooltipWrapper;
